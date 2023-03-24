15abe916c78eb769f708704eee8658c9ebfdda4b
AntonioRoccoGit <antonio.rocco97@gmail.com>
2023-03-24 01:56:40 +0100

    refactor code, create  functions that change icon from pause to play and reverse

--- js/script.js
+++ js/script.js
@@ -4,22 +4,22 @@ const filmImages = [
         image: 'img/01.webp',
         title: 'Marvel\'s Spiderman Miles Morale',
         text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, 
+    },
     {
         image: 'img/02.webp',
         title: 'Ratchet & Clank: Rift Apart',
         text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, 
+    },
     {
         image: 'img/03.webp',
         title: 'Fortnite',
         text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, 
+    },
     {
         image: 'img/04.webp',
         title: 'Stray',
         text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, 
+    },
     {
         image: 'img/05.webp',
         title: "Marvel's Avengers",
@@ -27,52 +27,59 @@ const filmImages = [
     }
 ];
 
-
-//BOTTONE START PAUSE
+//start pause btn
 const btnP = document.querySelector("#replay");
-console.log(btnP);
 btnP.addEventListener("click", startPause);
-//seleziono gli elementi del dom che andranno manipolati
-let sliderItemsContainer = document.querySelector(".slider-items"); //contenitore img
-let thumbNailsItems = document.querySelector(".thumbnails-image");
+
+//next prev btn
 const nextUp = document.querySelector(".next-up");// bottone next
 const prevDown = document.querySelector(".prev-down");//bottone prev
 
-//genero le grid dell'immagini
 imgInAGrid(filmImages);
 
-//setto stato iniziale della pagina
-let itemCounter = 0;
+//setting first displayed img
 const thumbNailsItemsArrey = document.querySelectorAll(".thumb-item");
 const sliderItemsArray = document.querySelectorAll(".item");
+
+let itemCounter = 0;
 sliderItemsArray[itemCounter].classList.add("active");
 thumbNailsItemsArrey[itemCounter].classList.add("active");
+
+//AUTO PLAY
+myAutoInterval = setInterval(goPreviousl, 3000);
 
 //impostiamo il bottone next
 nextUp.addEventListener("click", function () {
     goNext();
-    clearInterval(myAutoInterval);
-    myAutoInterval = setInterval(goNext, 3000);
+    if (myAutoInterval === null) {
+        iconPlayPause();
+    } else {
+        clearInterval(myAutoInterval);
+        myAutoInterval = setInterval(goNext, 3000);
+        iconPausePlay();
+    }
 });
 
 //imposto il bottone prev
 prevDown.addEventListener("click", function () {
-    //do al bottone la possibilita di tornrare indietro
     goPreviousl();
-    //blocco il vecchio intervallo
-    clearInterval(myAutoInterval);
-    myAutoInterval = setInterval(goPreviousl, 3000);
-
+    if (myAutoInterval === null) {
+        iconPlayPause();
+    } else {
+        clearInterval(myAutoInterval);
+        myAutoInterval = setInterval(goPreviousl, 3000);
+        iconPausePlay();
+    }
 });
-////////////////////////////////////////////////////////////////////////////////////////
-///////////////////////////////AUTO PLAY
-
-myAutoInterval = setInterval(goPreviousl, 3000);
 
 ////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////MY FUNCTION
-
+/**
+ * a special function used for generate my carousel layut
+ * @param {array} myImgArray 
+ */
 function imgInAGrid(myImgArray) {
+    let sliderItemsContainer = document.querySelector(".slider-items"); //contenitore img
 
     //grid img and thumbnail generator
     myImgArray.forEach((film, index) => {
@@ -84,29 +91,32 @@ function imgInAGrid(myImgArray) {
                     <p>${film.text}</p>
                 </section>
             </div>`;
-        
+
         thumbImg = document.createElement("div");
         thumbImg.classList.add("thumb-item");
         thumbImg.innerHTML = `<img src="${film.image}" alt="">`;
-        thumbImg.addEventListener("click", function() {
+        thumbImg.addEventListener("click", function () {
             clearInterval(myAutoInterval);
             hideDiscoverImg(index);
+            iconPausePlay();
             myAutoInterval = setInterval(goPreviousl, 3000);
 
         });
-        thumbNailsItems.append(thumbImg);
+        document.querySelector(".thumbnails-image").append(thumbImg);
     });
 };
 
 function goPreviousl() {
+    iconPausePlay();
     if (itemCounter === 0) {
-       hideDiscoverImg((sliderItemsArray.length - 1));
+        hideDiscoverImg((sliderItemsArray.length - 1));
     } else {
         hideDiscoverImg((itemCounter - 1));
     };
 };
 
 function goNext() {
+    iconPausePlay();
     if (itemCounter === sliderItemsArray.length - 1) {
         hideDiscoverImg(0);
     } else {
@@ -115,30 +125,41 @@ function goNext() {
 };
 
 function startPause() {
-    innerBtn = document.querySelector("#replay-icon");
     if (myAutoInterval === null) {
         myAutoInterval = setInterval(goPreviousl, 3000);
-        innerBtn.classList.remove("fa-regular");
-        innerBtn.classList.remove("fa-circle-play");
-        innerBtn.classList.add("fa-solid");
-        innerBtn.classList.add("fa-pause");
-    }else {
+        iconPausePlay();
+    } else {
         clearInterval(myAutoInterval);
         myAutoInterval = null;
-        innerBtn.classList.remove("fa-solid");
-        innerBtn.classList.remove("fa-pause");
-        innerBtn.classList.add("fa-regular");
-        innerBtn.classList.add("fa-circle-play");
+        iconPlayPause();
     }
 };
 
-function hideDiscoverImg(counter){
-    
+function hideDiscoverImg(counter) {
+
     sliderItemsArray[itemCounter].classList.remove("active");
     thumbNailsItemsArrey[itemCounter].classList.remove("active");
 
-    itemCounter = counter; 
+    itemCounter = counter;
 
     sliderItemsArray[itemCounter].classList.add("active");
     thumbNailsItemsArrey[itemCounter].classList.add("active");
 };
+
+function iconPlayPause() {
+    innerBtn = document.querySelector("#replay-icon");
+
+    innerBtn.classList.remove("fa-solid");
+    innerBtn.classList.remove("fa-pause");
+    innerBtn.classList.add("fa-regular");
+    innerBtn.classList.add("fa-circle-play");
+};
+
+function iconPausePlay() {
+    innerBtn = document.querySelector("#replay-icon");
+
+    innerBtn.classList.remove("fa-regular");
+    innerBtn.classList.remove("fa-circle-play");
+    innerBtn.classList.add("fa-solid");
+    innerBtn.classList.add("fa-pause");
+}
